23f2df1d22b75793203ea92562d81d114d330814
Ryo Muta <50309138+r-mutax@users.noreply.github.com>
2024-05-26 09:04:17 +0000

    step1 base merge

---
+++ driver/dummy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "util.h"
+#include "net.h"
+
+#define DUMMY_MTU UINT16_MAX /* maximum size of IP datagram */
+static int dummy_transmit(struct net_deevice * dev, uint16_t type, const uint8_t *data, size_t len, const void *dst){
+
+}
+
+static struct net_device_ops dummy_ops = {
+    .transmit = dummy_transmit,
+};
+
+static struct net_device_ops *dummy_init(void){
+
+}
---
+++ net.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "platform.h"
+
+#include "util.h"
+#include "net.h"
+
+/* NOTE: if you want to add/delete the entries after net_run(), you need to protect these lists with a mutex. */
+// net_run()より後にエントリを追加/削除する場合は、これらのリストをmutexで保護する必要があります。
+
+static struct net_device *devices;
+
+struct net_device *net_device_alloc(void)
+{
+
+}
+
+
+/* NOTE: must not be call after net_run() */
+// net_run()の後に呼び出してはいけません。
+int net_device_register(struct net_device *dev){
+
+}
+
+static int net_device_open(struct net_device *dev){
+
+}
+
+static int net_device_close(struct net_device *dev){
+
+}
+
+int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst){
+
+}
+
+int net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev){
+
+}
+
+int net_run(void){
+
+}
+
+void net_shutdown(void){
+
+}
+
+int net_init(void){
+
+}
